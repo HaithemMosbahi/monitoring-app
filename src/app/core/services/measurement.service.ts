6f719c9af8be2304ac74d8f253b7ebf7382cf983
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-10-01 15:47:38 +0100

    add measurement service and data

---
+++ src/app/core/services/measurement.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class MeasurementService {
+
+    constructor(private http: Http) { }
+
+    getWeightMeasurement(): Observable<any> {
+        return this.http.get('assets/data.json').map(response => response.json().weight);
+    }
+
+    getTemperatureMesasurement(): Observable<any> {
+        return this.http.get('assets/data.json').map(response => response.json().temperature);
+    }
+
+    getPainLevelMeasurement(): Observable<any> {
+        return this.http.get('assets/data.json').map(response => response.json().pain);
+    }
+}
