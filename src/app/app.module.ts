5b5c2e7a5e8f65b84f27d041525a25d5ee41cd67
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-10-01 12:29:42 +0100

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
+import { MonitoringModule } from './monitoring/monitoring.module';
+import { HttpModule } from '@angular/http';
+import { CustomRouterStateSerializer } from './shared/utils/router-utils';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './core/containers/app.component';
 
-import { AppComponent } from './app.component';
+import {AvatarModule} from 'ngx-avatar'
+import { environment } from '../environments/environment';
+
+import { MomentModule } from 'angular2-moment';
+
+
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer,
+} from '@ngrx/router-store';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+// App related imports 
+import { reducers, metaReducers } from "./core/reducers";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    CoreModule.forRoot(),
+    MonitoringModule,
+    HttpModule,
+
+    /**
+     * ngrx store root 
+     */
+    StoreModule.forRoot(reducers, { metaReducers }),
+    /**
+   * @ngrx/router-store keeps router state up-to-date in the store.
+   */
+    StoreRouterConnectingModule,
+
+    // Add side effects 
+    EffectsModule.forRoot([]),
+
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+
+    AvatarModule,
+    MomentModule
   ],
-  providers: [],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+   
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
