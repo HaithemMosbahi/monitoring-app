3527bc0f1362e426dbf1013b5bc3d02b9320918f
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-10-02 23:58:40 +0100

    add express API

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,17 @@
 // TO DO : add cloud functions to generate monitoring data
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express()
+app.get("/api/hello", (request, response) => {
+    response.send("Hello from Express on Firebase Functions!")
+});
+
+app.get("/measurements", (request, response) => {
+    const startDate = request.query.from;
+    const endDate = request.query.to;
+    response.send(`Generate measurements from ${startDate} to ${endDate}`);
+});
+
+// export the api 
+exports.app = functions.https.onRequest(app)
