d8dba8ecc218910e176bc56f1185de89a5c905a0
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-10-07 22:43:55 +0100

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  functions: {
+    app: {
+      url: 'https://us-central1-health-monitoring-c4a62.cloudfunctions.net/app'
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  functions:{
+    app:{
+      url:'http://localhost:5000/health-monitoring-c4a62/us-central1/app'
+    }
+  }
 };
